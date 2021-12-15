f1df39e92496bdae608c3b6942df019d15b7d0c3
Pippex <70782473+Pippex@users.noreply.github.com>
2021-12-15 13:43:50 -0600

    I add a new folder with the work of the first lesson

---
+++ lesson_1/lesson_1.py
@@ -0,0 +1,14 @@
+name = "Felipe"
+name2 = "Constanza"
+
+if name == name2:
+    print("Ambos tienen el mismo nombre")
+
+else:
+    print(f"{name} no se llama igual que {name2}")
+
+age = 15
+age2 = 15
+
+if age == age2:
+    print(f"{name} tiene la misma edad que {name2}")
