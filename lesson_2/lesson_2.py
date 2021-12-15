4532368c8a853bbb36ead74d3f86fcc5b431a196
Pippex <70782473+Pippex@users.noreply.github.com>
2021-12-15 14:24:37 -0600

    In this lesson I learn about functions and I have some functions in this file

---
+++ lesson_2/lesson_2.py
@@ -0,0 +1,30 @@
+def favorite(thing, thing_type):
+    print(f"{thing} is my favorite {thing_type}")
+
+
+favorite("Bitcoin Cash", "Cryptocurrency")
+favorite("Constanza", "girl")
+
+def constanza_is(quality):
+    print(f"Constanza is {quality}")
+
+
+constanza_is("fifteen years old")
+
+
+def comparate_ages(name_1, age_1, name_2, age_2):
+    
+    
+    if age_1 > age_2:
+        print(f"{name_1} is {age_1 - age_2} years older than {name_2}")
+    
+
+    elif age_1 == age_2:
+        print(f"{name_1} is the same age as {name_2}")
+
+
+    else:
+        print(f"{name_2} is {age_2 - age_1} years older than {name_1}")
+
+
+comparate_ages("Felipe", 15, "Constanza", 15)
