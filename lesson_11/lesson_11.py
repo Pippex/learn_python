09e1156af55793cf907275b84cea4ba2a7b983c0
Pippex <70782473+Pippex@users.noreply.github.com>
2021-12-27 12:55:29 -0600

    I added the method __init__ to my class house

--- lesson_11/lesson_11.py
+++ lesson_11/lesson_11.py
@@ -1,24 +1,33 @@
-class house():
-    surface = 100
-    color = "Red"
-    doors = 3
-    trees = 10
-    water_service = True
-    internet_service = False
-    electric_service = True
+from typing import Container
 
-    def contract_internet(self, contract_status=internet_service):
-        if type(contract_status) == bool:
-            self.internet_service = contract_status
+
+class house():
+
+    def __init__(self):
+        self.surface = 100
+        self.color = "Red"
+        self.doors = 3
+        self.trees = 10
+        self.water_service = True
+        self.internet_service = False
+        self.electric_service = True
+
+
+    def contract_internet(self, *args):
+        if len(args) == 1:
+            if type(args[0]) == bool:
+                self.internet_service = args[0]
         
         if self.internet_service:
             return "You have internet service"
         else:
             return "You don't have internet service"
 
+
     def create_door(self, doors_to_create=0):
         self.doors += doors_to_create
         return f"This house has {self.doors} doors"
+
 
     def status(self):
         characteristics = {"Surface":self.surface,
@@ -39,4 +48,4 @@ print(my_house.color)
 #print(my_house.rooms)
 my_house.create_door()
 print(my_house.doors)
-print(my_house.how_many_doors())
+print(my_house.contract_internet(True))
