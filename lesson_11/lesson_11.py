0ecf9de2f8f277405620bf0b924e1709a95f977d
Pippex <70782473+Pippex@users.noreply.github.com>
2021-12-27 12:41:09 -0600

    I created a method called status, this method returns a dictionary with the characteristics of the object. I optimize my code, too

--- lesson_11/lesson_11.py
+++ lesson_11/lesson_11.py
@@ -3,19 +3,40 @@ class house():
     color = "Red"
     doors = 3
     trees = 10
+    water_service = True
+    internet_service = False
+    electric_service = True
 
-    def create_door(self):
-        self.doors += 1
+    def contract_internet(self, contract_status=internet_service):
+        if type(contract_status) == bool:
+            self.internet_service = contract_status
+        
+        if self.internet_service:
+            return "You have internet service"
+        else:
+            return "You don't have internet service"
 
-    def how_many_doors(self):
-        return f"This object has {self.doors} doors"
+    def create_door(self, doors_to_create=0):
+        self.doors += doors_to_create
+        return f"This house has {self.doors} doors"
+
+    def status(self):
+        characteristics = {"Surface":self.surface,
+                           "Color":self.color,
+                           "Trees":self.trees,
+                           "Water Service":self.water_service,
+                           "Internet Service":self.internet_service,
+                           "Electric Service":self.electric_service}
+        
+        return characteristics
+    
 
 my_house = house()
 print(my_house.color)
 my_house.color = "Green"
 print(my_house.color)
-my_house.rooms = 10
-print(my_house.rooms)
+#my_house.rooms = 10
+#print(my_house.rooms)
 my_house.create_door()
 print(my_house.doors)
 print(my_house.how_many_doors())
