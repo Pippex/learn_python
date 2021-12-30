e7d81bc03d799f88370642de0e14e354d21c7ce4
Pippex <70782473+Pippex@users.noreply.github.com>
2021-12-30 14:44:10 -0600

    I optimze my function to find all items in a string and use it to create a new function that check if a email address is correct or not

--- lesson_13/lesson_13.py
+++ lesson_13/lesson_13.py
@@ -20,18 +20,37 @@ def position_string(string, item_searched, only_item=False):
     item_positions = []
     for i in range(len(string)):
         if string[i] == item_searched:
-            if only_item and i == len(string) - 1:
-                if string[i-1] == " ":
-                    item_positions.append(i)
-            elif only_item and i == 0:
-                if string[i+1] == " ":
-                    item_positions.append(i)
-            elif only_item:
-                if string[i-1] == string[i+1] == " ":
-                    item_positions.append(i)
+            if only_item and i == len(string) - 1 and string[i-1] == " ":
+                item_positions.append(i)
+            elif only_item and i == 0 and string[i+1] == " ":
+                item_positions.append(i)
+            elif only_item and string[i-1] == string[i+1] == " ":
+                item_positions.append(i)
+            elif not only_item:
+                item_positions.append(i)
     
     return item_positions
     
 print(position_string("a a a a a a a a a  a  a a  a    a", "a", only_item=True))
 
 #Ugh, this is a lot of if's, I wil try to optimize it
+#Mmn, this is not "Optimize", but at least it no longer has a lot of ifs
+
+print("aa 3".isalnum())
+
+#I'll create a function to comprobate if a email address is correct or not
+
+def correct_email(email):
+    has_at = False
+    ats_all = position_string(email, "@")
+    if len(ats_all) == 1:
+        if 0 != ats_all[0] and ats_all[0] != len(email)-1:
+            has_at = True
+
+    return has_at
+
+if correct_email(input("Please input your email address: ")):
+    print("Your email is correct")
+
+else:
+    print("Your email isn't correct")
